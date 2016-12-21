6deecae5eb6447ba2f3c66b91585ccc8a89be3a7
Auri <aurieh@users.noreply.github.com>
2016-12-21 19:06:51 +0200

    Hello classes my old friend

--- src/lib/spotify.js
+++ src/lib/spotify.js
@@ -1,21 +1,25 @@
 const ee = require('eventemitter3')
+
+global.Promise = require('bluebird');
 const request = require('request-promise')
+
 class Spotify extends ee {
-  constructor() {
-    this.client.trackCount = this.trackCount;
-    this.client.pollInterval = this.pollInterval;
-    this.client.username = this.username;
-    this.client.key = this.key;
+  constructor(options = {}) {
+    super();
+    this.trackCount = options.trackCount;
+    this.pollInterval = options.pollInterval;
+    this.username = options.username;
+    this.key = options.key;
   }
   requestHandler() {
-    let options = {
+    const options = {
       uri: `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks
       &user=\`${this.username}\`
       &api_key=\`${this.key}\`
       &format=json`,
       method: 'GET'
     }
-    request(options).then( (body) => {
+    return request(options).then( (body) => {
       let track = JSON.parse(body).recenttracks.track,
         parseTrack = []
       for(let i = 0; i < this.trackCount; i++) {
@@ -23,17 +27,18 @@ class Spotify extends ee {
           name: track[i].name,
           artist: track[i].artist["#text"]
         })
-        this.song(parseTrack)
       }
-    }).catch(reason) {
-      done(reason)
-    }
+      return Promise.resolve(parseTrack);
+    });
   }
   listen(){
-    this.requestHandler()
+    this.song();
     setInterval(() => {
-      this.emit('trackChange', this.requestHandler())
-    }, this.pollInterval)
+      this.song();
+    }, this.pollInterval);
+  }
+  song() {
+    this.requestHandler().then(o => this.emit('trackChange', o), console.error);
   }
 }
 module.exports = Spotify;
